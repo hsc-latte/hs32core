b8f84c16c00e9ee02e9af26bdf0bd0a748d5cd1e
Anthonykung <chiefanthony@hailiga.org>
2020-10-31 03:25:11 -0700

    Updated Execution Module
    - Matched I/O with Decode Module

--- rtl/cpu/hs32_exec.v
+++ rtl/cpu/hs32_exec.v
@@ -5,24 +5,28 @@
 */
 
 module hs32_exec (
-    input clk, input reset,
+    input clk,                       // 12 MHz Clock
+    input reset_n,                   // Active Low Reset
 
     // Fetch
     output  reg [31:0] newpc,   // New program
     output  reg flush,          // Flush
 
     // Decode
-    output  reg  reqd,          // Valid input
-    input   wire acke,          // Valid output
-    input   wire [2:0]  aluop,  // ALU Operation
-    input   wire [15:0] imm16,  // Immidiate
-    input   wire [3:0]  regsrc, // Register Source
-    input   wire [3:0]  regdst, // Register Destination
-    input   wire [15:0] ctlsig, // Control signals
+    input wire  [2:0]  aluop,  // ALU Operation
+    input wire  [15:0] imm16,  // Immidiate 16 bits
+    input wire  [15:0] imm5,  // Immidiate 5 bits
+    input wire  [15:0] imm24,  // Immidiate 24 bits
+    input wire  [3:0]  regdst, // Register Destination Rd
+    input wire  [3:0]  regsrc, // Register Source Rm
+    input wire  [3:0]  regopd, // Register Operand Rn
+    input wire  [15:0] ctlsig  // Control signals
 
     // Interrupts
     input   wire intrq,         // Interrupt signal
     input   wire [31:0] addi    // Interrupt address
 );
+
+    
 
 endmodule
