bde0410ddbddf639077cacb0c2fd4f5e68b46c9a
Anthonykung <chiefanthony@hailiga.org>
2020-11-01 11:56:50 -0800

    Update SRAM Testbench
    - Fixed compiling errors
    - Optimized tests

--- rtl/bench/frontend/tb_sram.v
+++ rtl/bench/frontend/tb_sram.v
@@ -1,6 +1,6 @@
 `include "frontend/sram.v"
 
-`timescale 1ns / 1ps
+`timescale 1ns / 100ps
 
 module TB_EXT_SRAM;
 
@@ -11,6 +11,106 @@ module TB_EXT_SRAM;
   reg [15:0] dtw;     // Data to write
   reg [15:0] din;     // Data in
 
+  initial begin
+    clk = 0;
+    #10
+    forever clk = #10 ~clk;
+  end
+
+  initial begin
+    $dumpfile("sram_tb.vcd");
+    $dumpvars(0,sram);
+
+    // Initial Reset
+    $display("%0t, Reset", $time);
+    clk = 0;
+    valid = 0;
+    rw = 0;
+    addri = 32'd0;
+    dtw = 16'd0;
+    din = 16'd0;
+
+    repeat (10) begin
+      /*************
+       * READ MODE *
+       *************/
+      $display("%0t, READ MODE", $time);
+
+      // Cycle 1
+      $display("%0t, Cycle 1", $time);
+      @ (posedge clk);
+      valid = 1;
+      rw = 0;
+      addri = 32'hFFFFFFFF;
+      dtw = 16'd0;
+      din = 16'd0;
+
+      // Cycle 2
+      $display("%0t, Cycle 2", $time);
+      @ (posedge clk);
+      valid = 1;
+      rw = 0;
+      addri = 32'hFFFFFFFF;
+      dtw = 16'd0;
+      din = 16'd0;
+
+      // Cycle 3
+      $display("%0t, Cycle 3", $time);
+      @ (posedge clk);
+      valid = 1;
+      rw = 0;
+      addri = 32'd0;
+      dtw = 16'd0;
+      din = 16'hFFFF;
+
+      // Cycle 4
+      $display("%0t, Cycle 4", $time);
+      @ (posedge clk);
+      valid = 0;
+      rw = 0;
+      addri = 32'd0;
+      dtw = 16'd0;
+      din = 16'd0;
+
+      /**************
+       * WRITE MODE *
+       **************/
+
+      // Cycle 1
+      @ (posedge clk);
+      valid = 1;
+      rw = 1;
+      addri = 32'hFFFFFFFF;
+      dtw = 16'hFFFF;
+      din = 16'd0;
+
+      // Cycle 2
+      @ (posedge clk);
+      valid = 1;
+      rw = 1;
+      addri = 32'hFFFFFFFF;
+      dtw = 16'hFFFF;
+      din = 16'd0;
+
+      // Cycle 3
+      @ (posedge clk);
+      valid = 1;
+      rw = 1;
+      addri = 32'hFFFFFFFF;
+      dtw = 16'hFFFF;
+      din = 16'd0;
+
+      // Cycle 4
+      @ (posedge clk);
+      valid = 0;
+      rw = 0;
+      addri = 32'd0;
+      dtw = 16'd0;
+      din = 16'd0;
+    end
+    $finish;
+  end
+
   EXT_SRAM sram(
     .clk(clk),
     .valid(valid),
@@ -19,174 +119,4 @@ module TB_EXT_SRAM;
     .dtw(dtw),
     .din(din)
   );
-
-  `ifdef SIMULATION
-
-    initial begin
-      clk = 0;
-      valid = 0;
-      rw = 0;
-      addri = 32'd0;
-      dtw = 16'd0;
-      din = 16'd0;
-      $dumpfile("sram_tb.vcd");
-      $dumpvars(0, sram);
-      #20
-      $finish;
-    end
-
-    always begin
-      #10 clk = ~clk;
-    end
-
-    reg [2:0] count = 3'd0;
-
-    always @(posedge clk) begin
-      count <= count + 1;
-    end
-
-    /*************
-     * READ MODE *
-     *************/
-
-    // Cycle 1
-    #20;
-    valid = 1;
-    rw = 0;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'd0;
-    din = 16'hFFFF;
-
-    // Cycle 2
-    #20;
-    valid = 1;
-    rw = 0;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'd0;
-    din = 16'hFFFF;
-
-    // Cycle 3
-    #20;
-    valid = 1;
-    rw = 0;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'd0;
-    din = 16'd0;
-
-    // Cycle 4
-    #20;
-    valid = 1;
-    rw = 0;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'd0;
-    din = 16'd0;
-
-    /**************
-     * WRITE MODE *
-     **************/
-
-    // Cycle 1
-    #20;
-    valid = 1;
-    rw = 1;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'hFFFF;
-    din = 16'd0;
-
-    // Cycle 2
-    #20;
-    valid = 1;
-    rw = 1;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'hFFFF;
-    din = 16'd0;
-
-    // Cycle 3
-    #20;
-    valid = 1;
-    rw = 1;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'hFFFF;
-    din = 16'd0;
-
-    // Cycle 4
-    #20;
-    valid = 1;
-    rw = 1;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'hFFFF;
-    din = 16'd0;
-
-    /*************
-     * READ MODE *
-     *************/
-
-    // Cycle 1
-    #20;
-    valid = 1;
-    rw = 0;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'd0;
-    din = 16'hFFFF;
-
-    // Cycle 2
-    #20;
-    valid = 1;
-    rw = 0;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'd0;
-    din = 16'hFFFF;
-
-    // Cycle 3
-    #20;
-    valid = 1;
-    rw = 0;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'd0;
-    din = 16'd0;
-
-    // Cycle 4
-    #20;
-    valid = 1;
-    rw = 0;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'd0;
-    din = 16'd0;
-
-    /**************
-     * WRITE MODE *
-     **************/
-
-    // Cycle 1
-    #20;
-    valid = 1;
-    rw = 1;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'hFFFF;
-    din = 16'd0;
-
-    // Cycle 2
-    #20;
-    valid = 1;
-    rw = 1;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'hFFFF;
-    din = 16'd0;
-
-    // Cycle 3
-    #20;
-    valid = 1;
-    rw = 1;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'hFFFF;
-    din = 16'd0;
-
-    // Cycle 4
-    #20;
-    valid = 1;
-    rw = 1;
-    addri = 32'hFFFFFFFF;
-    dtw = 16'hFFFF;
-    din = 16'd0;
-  `endif
 endmodule
