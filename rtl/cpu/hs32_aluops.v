31ec9f3f68fe889fba61c7f729dac3525c9f1161
Anthonykung <hi@anth.dev>
2020-11-19 22:05:29 -0800

    Fixed ALUOP and OPCODES Naming Conflicts
    - Added prefix A for ALUOPS

--- rtl/cpu/hs32_aluops.v
+++ rtl/cpu/hs32_aluops.v
@@ -8,16 +8,16 @@
 `define HS32_ALUOPS
 
 
-`define HS32_MOV       4'b0001
-`define HS32_ADD       4'b0010
-`define HS32_SUB       4'b0011
-`define HS32_AND       4'b0100
-`define HS32_OR        4'b0101
-`define HS32_XOR       4'b0110
-`define HS32_NOT       4'b0111
+`define HS32A_MOV       4'b0001
+`define HS32A_ADD       4'b0010
+`define HS32A_SUB       4'b0011
+`define HS32A_AND       4'b0100
+`define HS32A_OR        4'b0101
+`define HS32A_XOR       4'b0110
+`define HS32A_NOT       4'b0111
 
 
-`define HS32_ADC       4'b1010
-`define HS32_SBC       4'b1011
+`define HS32A_ADC       4'b1010
+`define HS32A_SBC       4'b1011
 
 `endif
